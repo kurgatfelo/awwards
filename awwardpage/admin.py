d28e9d9d4f36a269c3d1961cbfa78189970485d9
felo <felix.kurgat@student.moringaschool.com>
2022-04-11 16:53:51 +0300

    added admin.py

---
+++ awwardpage/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Rates,Profile,Post
+
+
+# Register your models here.
+admin.site.register(Rates)
+admin.site.register(Profile)
+admin.site.register(Post)
